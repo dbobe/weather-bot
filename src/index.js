6e9b6144f48dac98ba2d7a82a66b8c7f0419676e
David Bobe <bobe.david@live.com>
2024-05-13 20:36:07 -0400

    executing command and refactoring

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
 
+//events
 const { clientReadyHandler } = require("./events/clientReady");
+const { interactionCreateHandler } = require("./events/interactionCreate");
 // commands
 const pingCommand = require("./commands/ping");
 
@@ -12,6 +14,7 @@ const client = new Client({
 client.commands = new Collection();
 client.commands.set(pingCommand.data.name, pingCommand);
 
-client.on(Events.ClientReady, clientReadyHandler);
+client.once(Events.ClientReady, clientReadyHandler);
+client.on(Events.InteractionCreate, interactionCreateHandler);
 
 client.login();
