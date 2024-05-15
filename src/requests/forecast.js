054a6d982b9c436d7947e663c535075a2ed0e9a9
David Bobe <bobe.david@live.com>
2024-05-14 22:30:16 -0400

    Forecast command started

---
+++ src/requests/forecast.js
@@ -0,0 +1,45 @@
+const axios = require("axios");
+
+const URL = "https://api.weatherapi.com/v1/forecast.json";
+const FORECAST_DAYS = 3;
+
+async function fetchForecast(location) {
+  return await axios({
+    url: URL,
+    method: "get",
+    params: {
+      q: location,
+      days: FORECAST_DAYS,
+      key: process.env.WEATHER_API_KEY,
+    },
+  })
+    .then((response) => {
+      const city = response.data.location.name;
+      const state = response.data.location.region;
+      const country = response.data.location.country;
+      const locationName = `${city}, ${state}, ${country}`;
+
+      const weatherData = response.data.forecast.forecastday.map(
+        (forecastday) => {
+          return {
+            date: forecastday.date,
+            temperatureMinC: forecastday.day.mintemp_c,
+            temperatureMaxC: forecastday.day.maxtemp_c,
+            temperatureMinF: forecastday.day.mintemp_f,
+            temperatureMaxF: forecastday.day.maxtemp_f,
+          };
+        }
+      );
+      return {
+        locationName,
+        weatherData,
+      };
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+module.exports = {
+  fetchForecast,
+};
